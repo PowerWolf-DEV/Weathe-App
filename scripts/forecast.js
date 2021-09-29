60b16551f9944233564801b64193d9c7251cc523
PowerWolf-DEV <powerwolf131193@gmail.com>
2021-09-29 15:40:22 +0300

    created weather app

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const API = 'jfCY6lciuHwZk64FLlpZifIvGP66xzV4'
+
+// get weather info
+const getWeather = async (id) => {
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/'
+  const query = `${id}?apikey=${API}`
+
+  const response = await fetch(base + query)
+  const data = await response.json()
+
+  return data[0]
+
+}
+
+// get city ingo
+const getCity = async (city) => {
+
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'
+  const query = `?apikey=${API}&q=${city}`
+
+  const response = await fetch(base + query)
+  const data = await response.json()
+  
+  return data[0]
+
+}
+
